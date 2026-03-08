6ab4ee45d93e01aa75b85c041873b7b8c3b98373
Ahad <ahad.03k@gmail.com>
2026-03-08 17:26:57 +0000

    Updated files

--- project_sql/(3) top_demanded_skills.sql
+++ project_sql/(3) top_demanded_skills.sql
@@ -1,4 +1,18 @@
-SELECT *
+/* 
+Count of skills required for Data Analyst jobs in the United Kingdom, ordered by demand.
+ */
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short =  'Data Analyst' AND
+    job_location = 'United Kingdom'
+GROUP BY 
+    skills
+ORDER BY 
+    demand_count DESC
+LIMIT 5;
